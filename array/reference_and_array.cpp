7d8ea369f27ed2967d8ab2a3fe9a65ff0aca9ac2
logic-life <zstmr@mail.ustc.edu.cn>
2021-02-17 18:10:00 +0800

    reference_and_array.cpp

--- array/reference_and_array.cpp
+++ array/reference_and_array.cpp
@@ -23,15 +23,22 @@ int main()
     // cout << endl;
 
     // 数组初始化
+    //二维数组
     int v[4][2] = {0, 1, 2, 3, 4, 5};
-    int *i = v[0];
-    cout << "输出数组的第一个元素" << endl;
-    cout << i[0] << endl;
-    // for (int i = 0; i < 3; i++)
-    //     for (int j = 0; j < 2; j++)
-    //         cout << v[i][j] << " ";
-    cout << "打印数组中的元素" << endl;
-    for (auto k : v)
-        cout << *k << endl;
+    //前后类型一致均为数组指针，后者不能为v[2],因为类型为第三行的数组中的首元素指针，无法初始化一个数组指针
+    int(*i)[2] = &v[2];
+    cout << "查询v[1][1]类型" << typeid(v[1][1]).name() << endl; //二维数组中的一个元素
+    cout << "查询i的类型" << typeid(i).name() << endl;
+    cout << "查询&v[2]的类型" << typeid(&v[2]).name() << endl;
+    cout << "输出数组的第一个元素的地址" << endl;
+    cout << "输出v[0]的地址：" << v[0] << " 查询v[0]的类型" << typeid(v[0]).name() << endl; //其地址和首元素地址以及第零行数组的地址相同
+    cout << "输出v的地址：" << v << "   查询v类型：" << typeid(v).name() << endl;
+    cout << "输出*v的地址：" << *v << "  输出*v元素的类型：" << typeid(*v).name() << endl;
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 2; j++)
+            cout << "数组各元素地址为：" << (&v[i] + j) << "数组各元素值" << v[i][j] << endl;
+    cout << endl;
+    for (auto m : v) //赋给数组每行第一个元素的地址
+        cout << m << *m << endl;
     return 1;
 }
