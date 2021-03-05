2dae5440366c763e3353880892ac40bb7cfc3356
logic-life <zstmr@mail.ustc.edu.cn>
2021-03-05 10:38:31 +0800

    byreference

--- 数据结构与算法/链表/单链表/listnode_byreference.hpp
+++ 数据结构与算法/链表/单链表/listnode_byreference.hpp
@@ -6,35 +6,38 @@ struct ListNode
     int value;
     ListNode *next;
 };
-
+//添加节点
 void addTotail(ListNode *&p_head, int value)
-{
+{ //申请一个节点内存
     ListNode *new_node = new ListNode();
     new_node->next = nullptr;
     new_node->value = value;
+    //如果没有头结点就创建一个
     if (p_head == nullptr)
         p_head = new_node;
     else
-    {
+    { //在头结点后添加节点
         ListNode *p_node = p_head;
         for (; p_node->next != nullptr; p_node = p_node->next)
             ;
         p_node->next = new_node;
     }
 }
-
+//删除节点
 void RemoveNode(ListNode *&p_head, int value)
-{
+{ //头结点为空，无需删除
     if (p_head == nullptr)
         return;
+    //将待释放指针置为空
     ListNode *p_to_be_deleted = nullptr;
+    //判断头结点是否为要删除的节点
     if (p_head->value == value)
     {
         p_to_be_deleted = p_head;
         p_head = p_head->next;
     }
     else
-    {
+    { //遍历寻找待删除的节点
         ListNode *p_node = p_head;
         while (p_node->next != nullptr && p_node->next->value != value)
         {
@@ -52,6 +55,7 @@ void RemoveNode(ListNode *&p_head, int value)
         p_to_be_deleted = nullptr;
     }
 }
+//打印节点
 void ListPrintf(ListNode *&p_head)
 {
     ListNode *p_node = p_head;
