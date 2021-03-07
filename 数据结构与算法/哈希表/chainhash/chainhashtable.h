b4e8c25d2b0edc45537b30ff7d0161a653a12dbf
logic-life <zstmr@mail.ustc.edu.cn>
2021-03-07 14:54:09 +0800

    哈希表

--- 数据结构与算法/哈希表/chainhash/chainhashtable.h
+++ 数据结构与算法/哈希表/chainhash/chainhashtable.h
@@ -4,13 +4,16 @@
 
 using namespace std;
 typedef unsigned int uint;
-typedef struct Node
+struct Node
 {
-    const uint *key;
-    const string *value;
+    uint key;
+    string value;
     Node *next;
-
-} Node;
+    Node() : key(0), value(""), next(nullptr) {}
+    Node(const uint x) : key(x), value(""), next(nullptr) {}
+    Node(const uint x, const string str) : key(x), value(str), next(nullptr) {}
+    Node(const uint x, const string str, Node *next) : key(x), value(str), next(next) {}
+};
 
 class HashTable
 {
@@ -20,11 +23,11 @@ private:
 
 public:
     HashTable(/* args */);
-    uint hashFunction(const uint *key);
-    bool hashTableInsert(const uint *key, const string *value);
-    Node *hashTableSearch(const uint *key);
-    bool hashTableDelete(const uint *key);
-    //~HashTable();
+    uint hashFunction(const uint key);
+    bool hashTableInsert(const uint key, const string value);
+    Node *hashTableSearch(const uint key);
+    bool hashTableDelete(const uint key);
+    ~HashTable();
 };
 
 //生成一个HASHSIZE大小的哈希表
@@ -36,9 +39,9 @@ HashTable::HashTable()
     }
 }
 //定义哈希函数：Time33定义方式
-uint HashTable::hashFunction(const uint *key)
+uint HashTable::hashFunction(uint key)
 {
-    uint hash_value = *key % HASHSIZE;
+    uint hash_value = key % HASHSIZE;
     // for (; *key; key++)
     // {
     //     hash_value = hash_value * 33 + *key;
@@ -47,46 +50,100 @@ uint HashTable::hashFunction(const uint *key)
     return hash_value;
 }
 //哈希表查询操作
-Node *HashTable::hashTableSearch(const uint *key)
+Node *HashTable::hashTableSearch(const uint key)
 {
-    Node *np;
+
     uint index = hashFunction(key);
-    for (np = node[index]; np; np=np->next)
+    Node *np = node[index];
+    //如果表头为空，查询失败
+    if (np == nullptr)
+        return nullptr;
     {
-        if (*key == *(np->key))
+        //如果对应的键值在表头，返回表头
+        if (np->key == key)
             return np;
+        else
+        {
+            //指针顺着表头遍历查询，直至表结束。
+            for (; np->next; np = np->next)
+            { //找到关键字相同的数
+                if (key == (np->next->key))
+                    //返回查找成功的地址
+                    return np->next;
+            }
+            //未查找到对应的键值
+            return nullptr;
+        }
     }
-    return NULL;
 }
-//哈希表插入操作????????链表法插入有问题
-bool HashTable::hashTableInsert(const uint *key, const string *value)
+//哈希表插入操作
+bool HashTable::hashTableInsert(const uint key, const string value)
 {
     uint index;
     Node *np;
     //判断np对应节点的哈希值槽是否为空，空就插入一个新值
     if (!(np = hashTableSearch(key)))
-    {
-        index = hashFunction(key);
-        np = (Node *)malloc(sizeof(Node));
+    { //为新生的节点申请内存，如果堆区没有剩余空间，就返回插入失败
+        np = new Node(key, value);
         if (!np)
             return false;
-         np->key = key;
-         np->value = value;
-         np->next = node[index];                  
-         node[index] = np;
-
+        index = hashFunction(key);
+        //判断表头是否为空
+        if (node[index] == nullptr)
+        {
+            node[index] = np;
+        }
+        else
+        { //表头不为空时，就使用头插法，将节点插入在头结点的下一个位置
+            np->next = node[index]->next;
+            node[index]->next = np;
+        }
     }
-    np->value = value;
+    //返回插入成功
     return true;
 }
 
-
 //哈希表删除操作
-bool hashTableDelete(const uint *key)
+bool HashTable::hashTableDelete(const uint key)
 {
-    return false;
+    Node *np;
+    Node *pointer;
+    //查找要删除的键值
+    if (!(np = hashTableSearch(key)))
+        //未找到,np为NULL时，返回操作失败
+        return false;
+    else
+    { //删除元素为头结点
+        if (np == node[hashFunction(key)])
+        {
+            node[hashFunction(key)] = node[hashFunction(key)]->next;
+            delete np;
+        }
+        else
+        { //待删除元素不为头结点
+            for (pointer = node[hashFunction(key)]; pointer->next != np; pointer = pointer->next)
+                ;
+            pointer->next = np->next;
+            delete np;
+        }
+        return true;
+    }
 }
-// HashTable::~HashTable()
-// {
-//     delete[] node;
-// }
+
+HashTable::~HashTable()
+{
+    Node *pointer;
+    Node *p;
+    //遍历哈希表的表头
+    for (int i = 0; i < HASHSIZE; i++)
+    { //遍历哈希表表头指向的链表
+        for (pointer = node[i]; pointer;)
+        {
+            p = pointer;
+            //指向下一个节点
+            pointer = pointer->next;
+            //释放当前节点的内存
+            delete p;
+        }
+    }
+}
--- 数据结构与算法/哈希表/chainhash/test.cpp
+++ 数据结构与算法/哈希表/chainhash/test.cpp
@@ -1,38 +1,23 @@
 #include "chainhashtable.h"
-#include <map>
 #include <string.h>
 #include <vector>
 
 int main(int argc, char *argv[])
 {
-    map<uint, string> hash_map;
-
     vector<string> hash_vec = {"zhangsan", "lisi", "wangwu", "zhaoliu", "sunce", "lisanye"};
-
-    auto iterator = hash_vec.cbegin();
-    //使用向量初始化键值对
-    // for (uint i = 0; i < 6; i++)
-    //     hash_map.insert({i, *(iterator + i)});
-    hash_map.insert({1, hash_vec[1]});
-    hash_map.insert({11, hash_vec[2]});
-    hash_map.insert({2, hash_vec[3]});
-    hash_map.insert({3, hash_vec[4]});
-    hash_map.insert({4, hash_vec[5]});
-    hash_map.insert({14, hash_vec[6]});
-    map<uint, string>::iterator iter = hash_map.begin();
-    //创建哈希表
-    HashTable hashTable_1;
-    //为哈希表插入元素
-    for (iter; iter != hash_map.end(); iter++)
-        cout << hashTable_1.hashTableInsert(&(iter->first), &(iter->second)) << endl;
-    //查询key=3的值
-    uint key[] = {0, 1, 2, 3, 4, 5};
-
-    cout << *hashTable_1.hashTableSearch((key + 0))->value << endl;
-    cout << *hashTable_1.hashTableSearch((key + 1))->value << endl;
-    cout << *hashTable_1.hashTableSearch((key + 2))->value << endl;
-    cout << *hashTable_1.hashTableSearch((key + 3))->value << endl;
-    cout << *hashTable_1.hashTableSearch((key + 4))->value << endl;
-    cout << *hashTable_1.hashTableSearch((key + 5))->value << endl;
+    HashTable hash_name;
+    hash_name.hashTableInsert(1, hash_vec[1]);
+    hash_name.hashTableInsert(2, hash_vec[2]);
+    hash_name.hashTableInsert(12, hash_vec[3]);
+    hash_name.hashTableInsert(22, hash_vec[4]);
+    hash_name.hashTableInsert(3, hash_vec[5]);
+    hash_name.hashTableInsert(4, hash_vec[0]);
+    cout << hash_name.hashTableSearch(4)->value << endl;
+    cout << hash_name.hashTableSearch(3)->value << endl;
+    cout << hash_name.hashTableSearch(22)->value << endl;
+    cout << "删除一个元素" << endl;
+    cout << hash_name.hashTableDelete(22) << endl;
+    if (hash_name.hashTableSearch(22) == nullptr)
+        cout << "未查找到该元素" << endl;
     return 0;
 }
